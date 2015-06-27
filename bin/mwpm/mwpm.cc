081a66f61907d0fc2649dfbb5415826dce6b1dcc
Gary Pavlis <pavlis@indiana.edu>
2015-06-27 06:15:31 -0400

    Add stubs for programs for this package

---
+++ bin/mwpm/mwpm.cc
@@ -0,0 +1,10 @@
+/* This program produces uses the multiwavelet transform to compute particle 
+   motion ellipse estimates and errors for a suite of input seismograms */
+
+void usage()
+{
+    cerr << "mwpm db [-pf pffile]"<<endl;
+}
+int main(int argc, char **argv)
+{
+}
