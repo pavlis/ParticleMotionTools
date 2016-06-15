f17b7bc69bc7ee7f927f48efc977a1cb029964fa
pavlis <pavlis@indiana.edu>
2016-06-15 06:49:31 -0400

    Fix stupid mistake mixing up input and output

--- bin/seispp_filters/dbactive_reader/SourceData.cc
+++ bin/seispp_filters/dbactive_reader/SourceData.cc
@@ -9,18 +9,18 @@ using namespace std;
 SourceData::SourceData(const char* inp)
 {
   stringstream ss(inp);
-  ss << ffid;
   /* Note this blindly assumes time is set as an epoch time.   The
   test is used to handle this as this could easily happen. */
-  ss << time;
+  ss >> time;
+  ss >> ffid;
   if(ss.fail())
     throw SeisppError(string("SourceData constructor:  format error.\n")
                     + "Time field of input stream must be a epoch time.");
   /* Note these are assumed in degrees.   We convert the to radians to mesh
   with RegionalCoordinates */
-  ss << lat;
-  ss << lon;
-  ss << elev;
+  ss >> lat;
+  ss >> lon;
+  ss >> elev;
   lat=rad(lat);
   lon=rad(lon);
 }
