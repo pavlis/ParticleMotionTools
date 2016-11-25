eb4f64924e4a0d5253f0b0055083b113f65c5b66
Gary Pavlis <pavlis@indiana.edu>
2016-11-25 13:10:30 -0500

    New program for simple display of a 3C ensemble - end of pipeline

---
+++ bin/seispp_filters/display_ensemble/display_ensemble.cc
@@ -0,0 +1,79 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/text_iarchive.hpp>
+#include "seispp.h"
+#include "ThreeComponentSeismogram.h"
+#include "ensemble.h"
+#include "ThreeCEnsembleTimePicker.h"
+/* You will get lots of errors without these namespace
+   declaration*/
+using namespace std; 
+using namespace SEISPP; 
+void usage()
+{
+    cerr << "display_ensemble [-pf pffile] < infile"
+        <<endl
+        << "Display a ThreeComponentEnsemble object (typically end of a pipeline)"<<endl;
+    exit(-1);
+}
+template <class InputObject> InputObject 
+    read_object(boost::archive::text_iarchive& ia)
+{
+    InputObject d;
+    try{
+        ia>>d;
+    }catch(...)
+    {
+        throw SeisppError(string("read_object failed:  ")
+                    + "Check that input file is a boost text archive file");
+    }
+    return d;
+}
+bool SEISPP::SEISPP_verbose(true);
+int main(int argc, char **argv)
+{
+    int i;
+    if(argc>3) usage();
+    char *pffile=argv[0];
+    for(i=1;i<argc;++i)
+    {
+        string sarg(argv[i]);
+        if(sarg=="-pf")
+        {
+            ++i;
+            if(i>=argc)usage();
+            pffile=argv[i];
+        }
+        else
+            usage();
+    }
+    Pf *pf;
+    if(pfread(pffile,&pf))
+    {
+      cerr << "pfread failed on file="<<pffile<<endl;
+      usage();
+    }
+    try{
+        Metadata control(pf);
+        boost::archive::text_iarchive ia(cin);
+        ThreeComponentEnsemble d;
+        d=read_object<ThreeComponentEnsemble>(ia);
+        ThreeCEnsembleTimePicker win(control);
+        int nseis;
+        nseis=win.plot(d,true);
+        cout << "Plot completed of "<<nseis<<" seismogram"<<endl
+          << "Use the menu or type x in any active window to quit"
+          <<endl;
+    }catch(SeisppError& serr)
+    {
+        serr.log_error();
+    }
+    catch(std::exception& stexc)
+    {
+        cerr << stexc.what()<<endl;
+    }
+}
+
