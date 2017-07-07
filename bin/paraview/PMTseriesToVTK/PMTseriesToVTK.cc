6f6eed05228f5b718e0e454e539a7d5e1fd52530
pavlis <pavlis@indiana.edu>
2017-07-07 06:12:34 -0400

    Add support for serialized file and do some cleanup

--- bin/paraview/PMTseriesToVTK/PMTseriesToVTK.cc
+++ bin/paraview/PMTseriesToVTK/PMTseriesToVTK.cc
@@ -10,9 +10,20 @@
 #include "TimeWindow.h"
 #include "PfStyleMetadata.h"
 #include "AttributeMap.h"
+#include "StreamObjectReader.h"
 using namespace std;
 using namespace SEISPP;
-vector<PMTimeSeries> read_pmdata(string listfile)
+void usage()
+{
+    cerr << "PMTseriesToVTK infile [-s -pf pffile]"<<endl
+        << "infile is a serialized text file of PMTimeSeries objects or (optionally) a list of single files"<<endl
+        << "Use -s to switch to single file input model"<<endl
+        << "(In singe file mode infile should be a list of files with one seismogram per file)"<<endl
+        << "Use -pf to change parameter file to pffile instead of default PMTimeSeries.pf"
+        <<endl;
+    exit(-1);
+}
+vector<PMTimeSeries> read_pmdata_sfmode(string listfile)
 {
         vector<PMTimeSeries> result;
         ifstream lfin;
@@ -52,188 +63,190 @@ vector<PMTimeSeries> read_pmdata(string listfile)
         lfin.close();
         return result;
 }
-        
+vector<PMTimeSeries> read_pmdata_serial(string listfile)
+{
+  vector<PMTimeSeries> result;
+  try{
+      StreamObjectReader<PMTimeSeries> inp;
+      while(inp.good())
+      {
+          PMTimeSeries pmd=inp.read();
+          result.push_back(pmd);
+      }
+      return result;
+  }catch(...){throw;};
+}
 /* This procedure is in vtp_writer.cc*/
 void WriteEllipses(vector<PMTimeSeries>& d,
-        double t, double scale, int np_per_ellipse,
-                ofstream& out);
+    double t, double scale, int np_per_ellipse,
+        ofstream& out);
 
 void write_animation_files(vector<PMTimeSeries>& pmdv,
-        TimeWindow& dtw,double scale, int npoints_ellipse,
-        string dir, string fbase, string pvdfile)
+    TimeWindow& dtw,double scale, int npoints_ellipse,
+    string dir, string fbase, string pvdfile)
 {
-    const string base_error("write_animation_files:  ");
-    const string fext(".vtp");
-    try {
-        if(makedir(const_cast<char *>(dir.c_str())))
-            throw SeisppError(base_error 
-                    + "makedir failed to create directory="
-                    + dir);
-        int filenumber(1);  // file name tag - incremented for each 
-        list<string> allfnames;
-        string path;
-        ofstream fout;
-        /* assume first member is representative.   Caller
-           should assure input is rational and start times and
-           data lengths are equal or at least close.  */
-        int number_time_steps=trunc(dtw.length()/pmdv[0].dt);
-        double dt=pmdv[0].dt;
-        cout << "Program is writing data for "<< number_time_steps 
-            << " time steps at time intervals of "
-            << dt<<endl;
-        int i;
-        double t;
-        for(int i=0,t=dtw.start;i<number_time_steps;++i)
+  const string base_error("write_animation_files:  ");
+  const string fext(".vtp");
+  try {
+    if(makedir(const_cast<char *>(dir.c_str())))
+      throw SeisppError(base_error 
+          + "makedir failed to create directory="
+          + dir);
+    int filenumber(1);  // file name tag - incremented for each 
+    list<string> allfnames;
+    string path;
+    ofstream fout;
+    /* assume first member is representative.   Caller
+       should assure input is rational and start times and
+       data lengths are equal or at least close.  */
+    int number_time_steps=trunc(dtw.length()/pmdv[0].dt);
+    double dt=pmdv[0].dt;
+    cout << "Program is writing data for "<< number_time_steps 
+      << " time steps at time intervals of "
+      << dt<<endl;
+    int i;
+    double t;
+    for(int i=0,t=dtw.start;i<number_time_steps;++i)
+    {
+      stringstream ss;
+      ss << dir <<"/"<<fbase<<"_"<<filenumber<<fext;
+      string fname=ss.str();
+      fout.open(fname.c_str(),std::ofstream::out);
+      if(fout.fail())
+      {
+        cerr << base_error << "open failed for file="
+          <<fname<<endl
+          << "No output for time step "<<i<<endl;
+      }
+      else
+      {
+        try{
+          WriteEllipses(pmdv,dtw.start,scale,npoints_ellipse,fout);
+        }catch(SeisppError& serr)
         {
-            stringstream ss;
-            ss << dir <<"/"<<fbase<<"_"<<filenumber<<fext;
-            string fname=ss.str();
-            fout.open(fname.c_str(),std::ofstream::out);
-            if(fout.fail())
-            {
-                cerr << base_error << "open failed for file="
-                    <<fname<<endl
-                    << "No output for time step "<<i<<endl;
-            }
-            else
-            {
-                try{
-                    WriteEllipses(pmdv,dtw.start,scale,npoints_ellipse,fout);
-                }catch(SeisppError& serr)
-                {
-                    cerr << "WriteTimeWindow threw this error for time step "
-                        << i<<endl;
-                    serr.log_error();
-                    continue;
-                }
-                allfnames.push_back(fname);
-                ++filenumber;
-                dtw=dtw.shift(dt);
-                fout.close();
-            }
+          cerr << "WriteTimeWindow threw this error for time step "
+            << i<<endl;
+          serr.log_error();
+          continue;
         }
-        /* Now write the pvd file */
-        fout.open(pvdfile.c_str(),std::ofstream::out);
-        if(fout.fail())
-        {
-            cerr << "open failed for pvd file="<<pvdfile<<endl;
-            cerr << "You need to build this by hand or rerun"<<endl;
-            exit(-1);
-        }
-        /* preamble */
-        fout << "<?xml version=\"1.0\"?>"<<endl 
-            << "<VTKFile type=\"Collection\" version=\"0.1\" byte_order=\"LittleEndian\">"
-            <<endl
-            << "<Collection>"<<endl;
-        
-        list<string>::iterator iptr;
-        for(i=0,iptr=allfnames.begin();iptr!=allfnames.end();++iptr,++i)
-        {
-            fout << "<DataSet timestep=\"" << i <<"\" file=\""
-                << *iptr <<"\"/>" << endl;
-        }
-        fout << "</Collection>"<<endl
-            << "</VTKFile>"<<endl;
+        allfnames.push_back(fname);
+        ++filenumber;
+        dtw=dtw.shift(dt);
         fout.close();
-    }catch(...){throw;};
+      }
+    }
+    /* Now write the pvd file */
+    fout.open(pvdfile.c_str(),std::ofstream::out);
+    if(fout.fail())
+    {
+      cerr << "open failed for pvd file="<<pvdfile<<endl;
+      cerr << "You need to build this by hand or rerun"<<endl;
+      exit(-1);
+    }
+    /* preamble */
+    fout << "<?xml version=\"1.0\"?>"<<endl 
+      << "<VTKFile type=\"Collection\" version=\"0.1\" byte_order=\"LittleEndian\">"
+      <<endl
+      << "<Collection>"<<endl;
+    
+    list<string>::iterator iptr;
+    for(i=0,iptr=allfnames.begin();iptr!=allfnames.end();++iptr,++i)
+    {
+      fout << "<DataSet timestep=\"" << i <<"\" file=\""
+        << *iptr <<"\"/>" << endl;
+    }
+    fout << "</Collection>"<<endl
+      << "</VTKFile>"<<endl;
+    fout.close();
+  }catch(...){throw;};
 }
 double find_max_amplitude(PMTimeSeries& d)
 {
-    int i;
-    double maxamp(0.0);
-    //cout << d.ns << endl;
-    //vector<ParticleMotionEllipse> v;
-    //v = d.get_pmdata();
-    for(i=0;i<d.get_pmdata().size();++i)
-    {
-        ParticleMotionEllipse e=d.ellipse(i);
-        if(e.majornrm>maxamp) maxamp=e.majornrm;
-    }
-    return maxamp;
+  int i;
+  double maxamp(0.0);
+  //cout << d.ns << endl;
+  //vector<ParticleMotionEllipse> v;
+  //v = d.get_pmdata();
+  for(i=0;i<d.get_pmdata().size();++i)
+  {
+    ParticleMotionEllipse e=d.ellipse(i);
+    if(e.majornrm>maxamp) maxamp=e.majornrm;
+  }
+  return maxamp;
 }
 
 
 void show_scaling(vector<PMTimeSeries>& pmdv,
-        double initial_scale_factor)
+    double initial_scale_factor)
 {
-    vector<double> ranges,low,high,work;
-    double ampmax,awork;
-    int i;
-    vector<PMTimeSeries>::iterator dptr;
-    for(dptr=pmdv.begin(),i=0;dptr!=pmdv.end();++dptr,++i)
+  vector<double> ranges,low,high,work;
+  double ampmax,awork;
+  int i;
+  vector<PMTimeSeries>::iterator dptr;
+  for(dptr=pmdv.begin(),i=0;dptr!=pmdv.end();++dptr,++i)
+  {
+    if(i==0)
     {
-        if(i==0)
-        {
-            low.push_back(dptr->get_double("x"));
-            low.push_back(dptr->get_double("y"));
-            low.push_back(dptr->get_double("z"));
-            high=low;
-            work=low;
-            ampmax=find_max_amplitude(*dptr);
-        }
-        else
-        {
-            work[0]=dptr->get_double("x");
-            work[1]=dptr->get_double("y");
-            work[2]=dptr->get_double("z");
-            for(i=0;i<3;++i)
-            {
-                if(work[i]>high[i]) high[i]=work[i];
-                if(work[i]<low[i]) low[i]=work[i];
-            }
-            awork=find_max_amplitude(*dptr);
-            if(awork>ampmax) ampmax=awork;
-        }
+      low.push_back(dptr->get_double("x"));
+      low.push_back(dptr->get_double("y"));
+      low.push_back(dptr->get_double("z"));
+      high=low;
+      work=low;
+      ampmax=find_max_amplitude(*dptr);
     }
-    for(i=0;i<3;++i) 
-        ranges.push_back(high[i]-low[i]);
-    cout << "PMTimeSeriesToVTK:   Current scale factor="
-        << initial_scale_factor <<endl
-        << " produces a maximum, scaled particle motion amplitude = "
-        << ampmax*initial_scale_factor << endl
-        << "Here are coordinate ranges (low, high, extent):"  << endl;
-    for(i=0;i<3;++i)
-        cout << "x"<<i+1<<":  "<<low[i]<<", "<<high[i]<<", "<<ranges[i]<<endl;
+    else
+    {
+      work[0]=dptr->get_double("x");
+      work[1]=dptr->get_double("y");
+      work[2]=dptr->get_double("z");
+      for(i=0;i<3;++i)
+      {
+        if(work[i]>high[i]) high[i]=work[i];
+        if(work[i]<low[i]) low[i]=work[i];
+      }
+      awork=find_max_amplitude(*dptr);
+      if(awork>ampmax) ampmax=awork;
+    }
+  }
+  for(i=0;i<3;++i) 
+    ranges.push_back(high[i]-low[i]);
+  cout << "PMTimeSeriesToVTK:   Current scale factor="
+    << initial_scale_factor <<endl
+    << " produces a maximum, scaled particle motion amplitude = "
+    << ampmax*initial_scale_factor << endl
+    << "Here are coordinate ranges (low, high, extent):"  << endl;
+  for(i=0;i<3;++i)
+    cout << "x"<<i+1<<":  "<<low[i]<<", "<<high[i]<<", "<<ranges[i]<<endl;
 }
 void load_cartesian(vector<PMTimeSeries>& d, RegionalCoordinates& coords)
 {
-    try {
-        vector<PMTimeSeries>::iterator dptr;
-        for(dptr=d.begin();dptr!=d.end();++dptr)
-        {
-            double lat,lon,elev,r;
-            lat=dptr->get_double("site.lat");
-            lon=dptr->get_double("site.lon");
-            elev=dptr->get_double("site.elev");
-            /* Because of site qualifier assume these degree units */
-            lat=rad(lat);
-            lon=rad(lon);
-            r=r0_ellipse(lat)+elev;
-            Cartesian_point cp=coords.cartesian(lat,lon,r);
-            dptr->put("x",cp.x1);
-            dptr->put("y",cp.x2);
-            dptr->put("z",cp.x3);
-        }
-    }catch(...){throw;};
-}
-
-
-void usage()
-{
-    cerr << "PMTimeSeriesToVTK  inlist [-pf pffile]"<<endl
-        << "inlist is list of serialized file names for PMTimeSeries object"
-        <<endl
-        << "Use -pf to alter default control file PMTimeSeriesToVTK.pf"
-        <<endl;
-    exit(-1);
+  try {
+    vector<PMTimeSeries>::iterator dptr;
+    for(dptr=d.begin();dptr!=d.end();++dptr)
+    {
+      double lat,lon,elev,r;
+      lat=dptr->get_double("site.lat");
+      lon=dptr->get_double("site.lon");
+      elev=dptr->get_double("site.elev");
+      /* Because of site qualifier assume these degree units */
+      lat=rad(lat);
+      lon=rad(lon);
+      r=r0_ellipse(lat)+elev;
+      Cartesian_point cp=coords.cartesian(lat,lon,r);
+      dptr->put("x",cp.x1);
+      dptr->put("y",cp.x2);
+      dptr->put("z",cp.x3);
+    }
+  }catch(...){throw;};
 }
 bool SEISPP::SEISPP_verbose(false);
 int main(int argc, char **argv)
 {
-    if(argc<2) usage();
-    string infilelist(argv[1]);
-    string pffile("PMTimeSeriesToVTK.pf");
     int i;
+    if(argc<2) usage();
+    string infile(argv[1]);
+    bool singlefilemode(false);
+    string pffile("PMTimeSeriesToVTK.pf");
     for(i=2;i<argc;++i)
     {
         string sarg(argv[i]);
@@ -242,6 +255,10 @@ int main(int argc, char **argv)
             ++i;
             if(i>=argc) usage();
             pffile=string(argv[i]);
+        }
+        if(sarg=="-s")
+        {
+            singlefilemode=true;
         }
         else
             usage();
@@ -288,7 +305,11 @@ int main(int argc, char **argv)
         double PMscale_factor=md.get_double("PMscale_factor");
         /* Read the data defined by the input list of files */
         vector<PMTimeSeries>::iterator dptr;
-        vector<PMTimeSeries> d=read_pmdata(infilelist);
+        vector<PMTimeSeries> d;
+        if(singlefilemode)
+            d=read_pmdata_sfmode(infile);
+        else
+            d=read_pmdata_serial(infile);
         if(sap_mode)
         {
             /* In small aperture array mode we override any
