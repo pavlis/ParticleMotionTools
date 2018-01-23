04c65dccc9f0c7cf609be1af4618304221ce5632
Gary Pavlis <pavlis@indiana.edu>
2018-01-23 08:15:25 -0500

    Updates to matlab multiwavelet procedures to create pf file for mwpm

--- matlab/plotslepians.m
+++ matlab/plotslepians.m
@@ -1,41 +1,55 @@
-function  plotslepians( re, im,tstr )
-%plots pairs of slepian wavelets with one plot per pair
+function [ sumwtw,sumfw ] = plotslepians( re, im,dt,nstages, f_oversampling )
+%plots pairs of slepian wavelets along with time and frequency weight
+% functions in 3 subplots.   Last plot produces is overall frequency
+% weight in set of nstages divide by 2 decimators. 
+% f_oversampling is a multiplier used to zero pad prior to fft to 
+% improve appearance of frequency domain plots (has not effect on time
+% domain)
+% Returns overall time weighting function and frequency weighting
+% function
 sz=size(re);
 nw=sz(1,2);
 npts=sz(1,1);
-df=1/npts;
-f=0.0:df:0.5;   % to plot to Nyquist
+T=npts*dt;
+t=-(T-dt)/2:dt:T/2;
+df=1/(dt*npts);
+df=df/f_oversampling;
+fNy=1.0/(2.0*dt);
+f= -fNy:df:(fNy-df);
 nf=max(size(f));
+sumtw=zeros(npts,1);
+cw=zeros(nf,1);
+cwp=zeros(nf,1);
+sumfw=zeros(nf,1);
 figure;
 for i=1:nw
-    plot(re(:,i));
-    hold;
-    plot(im(:,i));
-    %if( i~= nw) 
-     %   figure;
-    %end
-    title(tstr);
+    % Don't worry about the phase for freq domain result - hence
+    % just stick the data in 1 to npts
+    cw(1:npts)=complex(re(:,i),im(:,i));
+    cwp(1:npts)=complex(im(:,i),re(:,i));
+    subplot(3,1,1),plot(t,re(:,i),'b',t,im(:,i),'r');
+    xlabel('time (s)');
+    title('wavelet');
+    subplot(3,1,2),plot(t,abs(cw(1:npts)));
+    xlabel('time (s)');
+    title('time weighting');
+    sumtw=sumtw+abs(cw(1:npts));
+    CW=fft(cw);
+    CW=fftshift(CW);
+    subplot(3,1,3),plot(f,abs(CW));
+    xlabel('Frequency (Hz)');
+    title('frequency weighting');
+    sumfw=sumfw+abs(CW);
+    % I think this will remove + and - irregularity (sin-cos term
+    % ambiguity)
+    CW=fft(cwp);
+    CW=fftshift(CW);
+    sumfw=sumfw+abs(CW);
     figure;
 end
-for i=1:nw
-    S=fft(re(:,i));
-    semilogy(f,abs(S(1:nf)));
-    if (i == 1) 
-        hold;
-    end
+plot(f,sumfw);
+hold;
+for i=1:nstages-1
+    f=f/2;
+    plot(f,sumfw);
 end
-title(tstr);
-figure;
-Ssum=zeros(npts,1);
-for i=1:nw
-    S=fft(im(:,i));
-    semilogy(f,abs(S(1:nf)));
-    if (i == 1)
-        hold;
-    end
-    Ssum=Ssum+abs(S);
-end
-title(tstr);
-figure;
-semilogy(f,Ssum(1:nf));
-title(tstr);
--- matlab/save_lpmwavelet_pf.m
+++ matlab/save_lpmwavelet_pf.m
@@ -1,4 +1,4 @@
-function [ output_args ] = save_lpmwavelet_pf( u, fc, p, dt, fname )
+function [ sumtw,sumfw ] = save_lpmwavelet_pf( u, fc, p, dt, nstages, f_oversampling, fname )
 %Creates a parameter file from multiwavlets stored in u.   It is
 % assumed the wavelets are adjacentpairs in columns of u that can
 % be placed in real and imaginary parts.   Always plots u as 
@@ -9,24 +9,69 @@ function [ output_args ] = save_lpmwavelet_pf( u, fc, p, dt, fname )
 % dt - sample interval used with fc in original calculation of wavelets
 % (note - the point is fc, p, and dt should be the same as those sent
 % to lpmwavelet.m script.)
+% nstages - has no effect on pf output, but displays series of frequency
+%   domain plots for nstages of divide by 2 decimation
+% f_oversampling - controls zero padding for frequency domain plots.  
+%   also has not effect on pf output, but improves the appearance of
+%   frequency domain plots
 % fname - output file name (string)
-plotlpmwavelet(u);
-title('multiwavelet pairs being saved');
-figure;
+%plotlpmwavelet(u);
+%title('multiwavelet pairs being saved');
+%figure;
 [m,n]=size(u);
+T=m*dt;
+t=-(T-dt)/2:dt:T/2;
 nw=n/2;
 for i=1:nw
     ii=2*(i-1)+1;
     cw(:,i)=complex(u(:,ii),u(:,ii+1));
+    cwp(:,i)=complex(u(:,ii+1),u(:,ii));
 end
+fNy=1.0/(2.0*dt);
+df=1/T;
+df=df/f_oversampling;
+fNy=1.0/(2.0*dt);
+f= -fNy:df:(fNy-df);
+nf=max(size(f));
+sumtw=zeros(m,1);
+sumfw=zeros(nf,1);
+cws=zeros(nf:1);
+
+f=-fNy:df:(fNy-df);
 for i=1:nw
-    subplot(2,1,1),plot(abs(cw(:,i)));
+    
+    subplot(3,1,1),plot(t,real(cw(:,i)),'b',t,imag(cw(:,i)),'r');  
+    xlabel('time (s)');
+    title('wavelet');
+    subplot(3,1,2),plot(t,abs(cw(:,i)));
+    xlabel('time (s)');
     title('time weighting');
-    CW=fft(cw(:,i));
-    subplot(2,1,2),plot(abs(CW));
+    sumtw=sumtw+abs(cw(:,1));
+    cws=zeros(nf,1);
+    cws(1:m)=cw(:,i);
+    CW=fft(cws);
+    CW=fftshift(CW);
+    subplot(3,1,3),plot(f,abs(CW));
+    xlabel('Frequency (Hz)');
     title('frequency weighting');
-    pause
+    sumfw=sumfw+abs(CW);
+    % I think this will remove + and - irregularity (sin-cos term
+    % ambiguity)
+    cws=zeros(nf,1);
+    cws(1:m)=cw(:,i);
+    CW=fft(cws);
+    CW=fftshift(CW);
+    sumfw=sumfw+abs(CW);
+    figure;
 end
+plot(f,sumfw);
+hold;
+for i=1:nstages-1
+    f=f/2;
+    plot(f,sumfw);
+end
+xlabel('Frequency (Hz)');
+title('Overall frequency weighting');
 fid=fopen(fname,'w');
 fprintf(fid,'nsamples    %d\n',m);
 fprintf(fid,'nwavelets   %d\n',nw);
